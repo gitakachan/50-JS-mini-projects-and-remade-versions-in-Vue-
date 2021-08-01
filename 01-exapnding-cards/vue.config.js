512d60404777c544794ca752ed5f05bb55ac78f1
Cheni Chen <celiachenic@gmail.com>
2021-08-01 23:30:59 +0800

    001

---
+++ 01-exapnding-cards/vue.config.js
@@ -0,0 +1,13 @@
+//這裡寫的配置會和其他配置結合
+module.exports = {
+  //配置別名
+  configureWebpack: {
+    resolve: {
+      alias: {
+        // "@": "src", //已經默認配置
+        assets: "@/assets",
+        components: "@/components",
+      },
+    },
+  },
+};
